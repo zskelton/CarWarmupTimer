2270d6359729107f670088d543b79bf400603cad
Zachary D. Skelton <zbert@hotmail.com>
2022-01-17 13:57:59 -0600

    API call setup

--- app/src/main/java/android/example/weathercartimer/FirstFragment.kt
+++ app/src/main/java/android/example/weathercartimer/FirstFragment.kt
@@ -1,5 +1,6 @@
 package android.example.weathercartimer
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,11 +8,35 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import android.example.weathercartimer.databinding.FragmentFirstBinding
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
+import com.android.volley.RequestQueue
+import com.android.volley.VolleyError
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    // Function to Run Web Query
+    private fun runQuery(view: View) {
+        val TAG = "Query";
+        val textView = view.findViewById<TextView>(R.id.txt_status)
+        // From: https://stackoverflow.com/questions/28599377/how-to-run-volley-in-fragmentnavigation-drawer
+        val url = "https://www.google.com"
+        val res = Response.Listener<String> { response -> Snackbar.make(view, "success: ${response}", Snackbar.LENGTH_LONG).setAction("Action", null).show(); Log.d(TAG, response) }
+        val eros = Response.ErrorListener { volleyError -> Snackbar.make(view,"volley error - $volleyError", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        val request = StringRequest(Request.Method.GET, url, res,eros)
+        val rQueue = Volley.newRequestQueue(requireActivity()!!.applicationContext)
+        rQueue.add(request)
+    }
 
     private var _binding: FragmentFirstBinding? = null
 
@@ -32,9 +57,11 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        binding.buttonFirst.setOnClickListener {
-//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        }
+        binding.btnStartTimer.setOnClickListener { view ->
+            runQuery(view)
+            Snackbar.make(view, "Done", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/android/example/weathercartimer/MainActivity.kt
+++ app/src/main/java/android/example/weathercartimer/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.example.weathercartimer.databinding.ActivityMainBinding
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,11 +28,6 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
-//        binding.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
